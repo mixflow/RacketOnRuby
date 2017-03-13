4411fc9d6692e19d559dbffc904e33613a84681b
MixFlow <wangweimf@gmail.com>
2017-03-14 00:30:38 +0800

    correct comment of split(' ')

--- racket.rb
+++ racket.rb
@@ -2,6 +2,6 @@ class Racket
     def tokenize(str)
        str.gsub("(", "( ") # add space after '('
            .gsub!(")", " )") # add space before ')'
-           .split(" ") # split string into an array(tokens) base on space
+           .split(" ") # split string into an array(tokens) base on whitespaces
     end
 end
